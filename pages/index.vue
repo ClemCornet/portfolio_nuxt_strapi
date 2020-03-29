969df4f2ba8ae63807809bdcbf2c3e01c12601f6
Cornet <cornet.clement@gmail.com>
2020-03-29 21:33:12 +0200

    init commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <div>
+      <h1>MA super page nuxt</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style>
+</style>
